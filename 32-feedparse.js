2538fd58ba5a2e3cc224d949f51771d23dc68389
okhiroyuki <ok.okada.hiroyuki@gmail.com>
2018-05-30 10:54:03 +0900

    merge 0.1.13

--- 32-feedparse.js
+++ 32-feedparse.js
@@ -1,3 +1,4 @@
+
 module.exports = function(RED) {
     "use strict";
     var FeedParser = require("feedparser");
@@ -8,12 +9,12 @@ module.exports = function(RED) {
     function FeedParseNode(n) {
         RED.nodes.createNode(this,n);
         this.url = n.url;
+        if (n.interval > 35790) { this.warn(RED._("feedparse.errors.invalidinterval")) }
         this.interval = (parseInt(n.interval)||15) * 60000;
         this.encode = n.encode;
         var node = this;
         this.interval_id = null;
         this.seen = {};
-        this.conv;
         var parsedUrl = url.parse(this.url);
         if (!(parsedUrl.host || (parsedUrl.hostname && parsedUrl.port)) && !parsedUrl.isUnix) {
             this.error(RED._("feedparse.errors.invalidurl"));
@@ -24,18 +25,19 @@ module.exports = function(RED) {
                 if(node.encode.length > 0){
                     req = request({url: node.url, encoding: 'binary', timeout: 10000, pool: false});
                 }else{
-                    req = request(node.url, {timeout: 10000, pool: false});
+                    req = request(node.url, {timeout:10000, pool:false});
                 }
                 //req.setMaxListeners(50);
-                //req.setHeader('user-agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36');
-                //req.setHeader('accept', 'text/html,application/xhtml+xml');
+                req.setHeader('user-agent', 'Mozilla/5.0 (Node-RED)');
+                req.setHeader('accept', 'text/html,application/xhtml+xml');
+
                 var feedparser = new FeedParser();
 
                 req.on('error', function(err) { node.error(err); });
 
                 req.on('response', function(res) {
                     if (res.statusCode != 200) { node.warn(RED._("feedparse.errors.badstatuscode")+" "+res.statusCode); }
-                    else {res.pipe(feedparser);}
+                    else { res.pipe(feedparser); }
                 });
 
                 feedparser.on('error', function(error) { node.error(error); });
@@ -43,16 +45,16 @@ module.exports = function(RED) {
                 feedparser.on('readable', function () {
                     var stream = this, article;
                     while (article = stream.read()) {  // jshint ignore:line
+                        if(node.encode.length > 0){
+                            var title = new Buffer(article.title, 'binary');
+                            article.title = iconv.decode(title,node.encode);
+                            var summary = new Buffer(article.summary, 'binary');
+                            article.summary = iconv.decode(summary,node.encode);
+                            var description  = new Buffer(article.description, 'binary');
+                            article.description = iconv.decode(description,node.encode);
+                        }
                         if (!(article.guid in node.seen) || ( node.seen[article.guid] !== 0 && node.seen[article.guid] != article.date.getTime())) {
                             node.seen[article.guid] = article.date?article.date.getTime():0;
-                            if(node.encode.length > 0){
-                                var title = new Buffer(article.title, 'binary');
-                                article.title = iconv.decode(title,node.encode);
-                                var summary = new Buffer(article.summary, 'binary');
-                                article.summary = iconv.decode(summary,node.encode);
-                                var description  = new Buffer(article.description, 'binary');
-                                article.description = iconv.decode(description,node.encode);
-                            }
                             var msg = {
                                 topic: article.origlink || article.link,
                                 payload: article.description,
@@ -78,4 +80,4 @@ module.exports = function(RED) {
     }
 
     RED.nodes.registerType("feedparse",FeedParseNode);
-}
+};
